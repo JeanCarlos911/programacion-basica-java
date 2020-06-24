f42a52eff72c6a5d46e2c10d44ec01a1cc779ad6
Jean Carlos Santoya Cabrera <jeancarlosodi@gmail.com>
2020-06-24 18:20:05 -0500

    se agrega solucion al ejercicio propuesto 014

---
+++ 014-Empleado/src/Empleado.java
@@ -0,0 +1,44 @@
+public class Empleado {
+    private String nombre, apellidoPaterno;
+    private double salarioMensual;
+    
+    public Empleado(String nombre, String apellido, double salario){
+        setNombre(nombre);
+        setApellidoPaterno(apellido);
+        setSalarioMensual(salario);
+    }
+    
+    public void setNombre(String nombre){
+        this.nombre = nombre;
+    }
+    
+    public void setApellidoPaterno(String apellido){
+        this.apellidoPaterno = apellido;
+    }
+    
+    public void setSalarioMensual(double salario){
+        this.salarioMensual = salario;
+    }
+    
+    public String getNombre(){
+        return nombre;
+    }
+    
+    public String getApellidoPaterno(){
+        return apellidoPaterno;
+    }
+    
+    public double getSalarioMensual(){
+        return salarioMensual;
+    }
+    
+    public void mostrarSalarioAnual(){
+        double salarioAnual = getSalarioMensual()*12;
+        
+        System.out.printf("El empleado %s %s tiene un salario anual de $%.2f\n", getNombre(), getApellidoPaterno(), salarioAnual);
+    }
+    
+    public void aumentarSalario(int porcentaje){
+        setSalarioMensual(getSalarioMensual() * (100 + porcentaje) / 100);
+    }
+}
